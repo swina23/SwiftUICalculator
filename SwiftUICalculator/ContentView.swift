5c064627b14187a82e78a1912196d4790d31bacd
tomo.h <tomo.the.slyb@gmail.com>
2020-01-10 20:31:25 +0900

    SwiftUI Calculator - Master Stacking (Ep 1)

--- SwiftUICalculator/ContentView.swift
+++ SwiftUICalculator/ContentView.swift
@@ -9,8 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+           
+            
+            VStack(spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text("42").foregroundColor(.white)
+                       .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                            .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                            .foregroundColor(.white)
+                            .background(Color.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
